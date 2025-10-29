2d2e116fea6dff5bc3ba86953d39c2a4a3950aa3
Wojciech Graj <me@w-graj.net>
2025-10-29 10:50:38 +0100

    Sanitize macro
    The clone macro would fail to work unless the user imported it,
    disallowing the following: clone_macro::clone!(/* ... */);

--- src/lib.rs
+++ src/lib.rs
@@ -165,25 +165,24 @@
 macro_rules! clone {
     () => {};
     ([$($tt:tt)*], $expr:expr) => {{
-        clone!($($tt)*);
-
+        $crate::clone!($($tt)*);
         $expr
     }};
     ($(,)? mut { $expr:expr } as $ident:ident $($tt:tt)*) => {
         let mut $ident = ::core::clone::Clone::clone(&$expr);
-        clone!($($tt)*);
+        $crate::clone!($($tt)*);
     };
     ($(,)? mut $ident:ident $($tt:tt)*) => {
         let mut $ident = ::core::clone::Clone::clone(&$ident);
-        clone!($($tt)*);
+        $crate::clone!($($tt)*);
     };
     ($(,)? { $expr:expr } as $ident:ident $($tt:tt)*) => {
         let $ident = ::core::clone::Clone::clone(&$expr);
-        clone!($($tt)*);
+        $crate::clone!($($tt)*);
     };
     ($(,)? $ident:ident $($tt:tt)*) => {
         let $ident = ::core::clone::Clone::clone(&$ident);
-        clone!($($tt)*);
+        $crate::clone!($($tt)*);
     };
     ($(,)?) => {};
 }
